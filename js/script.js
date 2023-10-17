1bc22a70131cabf544d1e61d1ca129480ffa64d4
albert ambani <albertambani1@gmail.com>
2023-10-17 17:51:43 +0300

    smooth scrolling

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// JavaScript
+// Toggle menubar
 document.addEventListener('DOMContentLoaded', function() {
     const menuButton = document.getElementById('menu-button');
     const menuBar = document.querySelector('header');
@@ -12,13 +12,34 @@ document.addEventListener('DOMContentLoaded', function() {
     menuBar.classList.remove('header-visible');
   }
 }) ;
-
     });
     
   });
 
- 
+ // Add a click event listener to the navigation links
+document.querySelectorAll('.navbar a').forEach(link => {
+  link.addEventListener('click', smoothScroll);
+});
 
+document.querySelectorAll('.home a').forEach(link => {
+  link.addEventListener('click', smoothScroll);
+});
+
+
+// Function to implement smooth scrolling
+function smoothScroll(e) {
+  e.preventDefault();
+
+  const targetId = this.getAttribute('href').substring(1); 
+  const targetElement = document.getElementById(targetId);
+
+  if (targetElement) {
+      targetElement.scrollIntoView({
+          behavior: 'smooth', // Use smooth scrolling
+          block: 'start', // Scroll to the top of the target element
+      });
+  }
+}
   
   
   
