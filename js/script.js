f460fa3f94b7a3c7663ea6081faa40ab8e1b22cd
albert ambani <albertambani1@gmail.com>
2023-11-19 14:03:23 +0300

    smtp implementation

--- js/script.js
+++ js/script.js
@@ -5,15 +5,19 @@ document.addEventListener('DOMContentLoaded', function() {
   
     menuButton.addEventListener('click', function() {
       menuBar.classList.toggle('header-visible');
+    }) 
 
- //close menubar when clicking outside
- document.body.addEventListener('click', function(event) {
-  if  (!event.target.closest('header') && !event.target.closest('#menu-button')) {
-    menuBar.classList.remove('header-visible');
-  }
-}) ;
-    });
+    // Add an event listener to the document body 
+document.body.addEventListener('click', function (event) {
+
+  // Check if the click target is not within the navbar or toggle button
+  if (!event.target.closest('header') && !event.target.closest('.menu-button')) {
     
+      // Close the menu by removing the "active" class
+      menuBar.classList.remove('header-visible');
+  }
+});
+
   });
 
  // Add a click event listener to the navigation links
@@ -40,6 +44,21 @@ function smoothScroll(e) {
       });
   }
 }
-  
-  
-  
+
+//function to send e-email
+function sendEmail() {
+  sendEmail.send({
+    Host : "smtp.gmail.com",
+    Username : "albertambani1@gmail.com",
+    Password : "@@lbZ@t1",
+    To : 'albertambani1@gmail.com' ,
+    From : document.getElementById("email").value,
+    Subject : "New contact form inquiry" ,
+    Body : "Name: " + document.getElementById("name").value
+           + "<br> Email: " + document.getElementById("email").value
+           + "<br> Phone no: " + document.getElementById("phone").value
+           + "<br> Message: " + document.getElementById("message").value
+  }).then(
+    message => alert("Message sent successfully!")
+  ); 
+}
